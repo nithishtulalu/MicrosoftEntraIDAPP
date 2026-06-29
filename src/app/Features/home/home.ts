905196f797959a67ea3fb877a2f362a7a87fee82
tulalunithish <nithishtulalu121@gmail.com>
2026-06-29 12:02:36 +0530

    feat(dashboard): add dashboard component with HTML, CSS, and tests

--- src/app/Features/home/home.ts
+++ src/app/Features/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Header } from '../Layout/header/header';
 import { Footer } from '../Layout/footer/footer';
 import { Sidebar } from '../Layout/sidebar/sidebar';
+import { Dashboard } from '../../app/Features/dashboard/dashboard/dashboard';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Header, Footer, Sidebar],
+  imports: [Header, Footer, Sidebar, Dashboard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
